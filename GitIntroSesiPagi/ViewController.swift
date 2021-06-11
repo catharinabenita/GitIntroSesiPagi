f0475501a6a7a23e715afc7be8da21a06061172e
Catharina Benita <benita_irawan@yahoo.co.id>
2021-06-11 09:03:22 +0700

    Initial Commit

---
+++ GitIntroSesiPagi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitIntroSesiPagi
+//
+//  Created by Catharina Benita Irawan on 11/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
